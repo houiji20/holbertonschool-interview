9ffecfc95034a3c1c7732437ab1b3acbf6d78ea1
houiji20 <91070531+houiji20@users.noreply.github.com>
2023-01-19 20:42:37 +0100

    Update lists.h

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -23,5 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int rec_pal(listint_t **left, listint_t *right);
+
 
 #endif /* LISTS_H */
